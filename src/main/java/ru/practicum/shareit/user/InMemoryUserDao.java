e70acc96ac05c6eb9ab5f47e0f880d94dbbb9a3a
Goggy <bigggrinz@gmail.com>
2026-06-14 21:35:16 +0500

    правки по ревью

--- src/main/java/ru/practicum/shareit/user/InMemoryUserDao.java
+++ src/main/java/ru/practicum/shareit/user/InMemoryUserDao.java
@@ -2,7 +2,6 @@ package ru.practicum.shareit.user;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Repository;
-import ru.practicum.shareit.exception.NotFoundException;
 
 import java.util.HashMap;
 import java.util.Map;
