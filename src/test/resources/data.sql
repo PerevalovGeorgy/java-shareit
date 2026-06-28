51907c89be5ba46d384e8ab7232b2e6b52eafb04
Goggy <bigggrinz@gmail.com>
2026-06-29 01:13:20 +0500

    правки

--- src/test/resources/data.sql
+++ src/test/resources/data.sql
@@ -1,4 +1,4 @@
-DELETE FROM users WHERE id >= 57;
+ALTER TABLE users ALTER COLUMN id RESTART WITH 1;
 
 INSERT INTO users (id, name, email) VALUES
-(57, 'Test User 57', 'test57@example.com'),
+(57, 'Test User 57', 'test57@example.com');
