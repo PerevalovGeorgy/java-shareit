632591a67182b95905b1cc4ddeacf9c67e79954d
Goggy <bigggrinz@gmail.com>
2026-06-29 01:19:26 +0500

    правки

--- src/test/resources/data.sql
+++ src/test/resources/data.sql
@@ -1,4 +1,4 @@
 ALTER TABLE users ALTER COLUMN id RESTART WITH 1;
 
 INSERT INTO users (id, name, email) VALUES
-(57, 'Test User 57', 'test57@example.com');
+(42, 'Test User 42', 'test42@example.com');
