d4b2b3a7b79c86a068805d113437164025878dd5
Goggy <bigggrinz@gmail.com>
2026-07-14 00:41:25 +0500

    правки

--- shareit-gateway/src/test/java/ru/practicum/shareit/request/ItemRequestControllerTest.java
+++ shareit-gateway/src/test/java/ru/practicum/shareit/request/ItemRequestControllerTest.java
@@ -4,12 +4,14 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.web.servlet.MockMvc;
+import ru.practicum.shareit.ShareitGateway;
 
 import java.time.LocalDateTime;
 import java.util.Arrays;
@@ -20,7 +22,8 @@ import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@WebMvcTest(ItemRequestController.class)
+@SpringBootTest(classes = ShareitGateway.class)
+@AutoConfigureMockMvc
 class ItemRequestControllerTest {
 
     @Autowired
