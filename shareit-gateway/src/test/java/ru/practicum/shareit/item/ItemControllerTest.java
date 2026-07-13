b35b9c3b462aaa4df1449b1ecc20216e8b379f22
Goggy <bigggrinz@gmail.com>
2026-07-14 00:21:11 +0500

    правки

--- shareit-gateway/src/test/java/ru/practicum/shareit/item/ItemControllerTest.java
+++ shareit-gateway/src/test/java/ru/practicum/shareit/item/ItemControllerTest.java
@@ -6,13 +6,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.context.annotation.Import;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.web.servlet.MockMvc;
-import ru.practicum.shareit.TestConfig;
-import ru.practicum.shareit.booking.BookingController;
 
 import java.util.Arrays;
 import java.util.List;
