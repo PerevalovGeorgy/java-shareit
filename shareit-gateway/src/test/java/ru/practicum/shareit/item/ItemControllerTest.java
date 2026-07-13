077a337dc7639fcfdfb07199a67581dfed8abb53
Goggy <bigggrinz@gmail.com>
2026-07-13 23:59:18 +0500

    правки

--- shareit-gateway/src/test/java/ru/practicum/shareit/item/ItemControllerTest.java
+++ shareit-gateway/src/test/java/ru/practicum/shareit/item/ItemControllerTest.java
@@ -4,14 +4,15 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.context.annotation.Import;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
-import ru.practicum.shareit.TestConfig;
+import ru.practicum.shareit.ShareItGateway;
 
 import java.util.Arrays;
 import java.util.List;
@@ -21,8 +22,9 @@ import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@WebMvcTest(ItemController.class)
-@Import(TestConfig.class)
+@SpringBootTest(classes = ShareItGateway.class)
+@AutoConfigureMockMvc
+@ContextConfiguration(classes = {ShareItGateway.class})
 class ItemControllerTest {
 
     @Autowired
