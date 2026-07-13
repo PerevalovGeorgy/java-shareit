a4e12194cbc3ea1c0d86e9440574d3e19fc1923b
Goggy <bigggrinz@gmail.com>
2026-07-13 23:47:50 +0500

    правки

--- shareit-gateway/src/test/java/ru/practicum/shareit/request/ItemRequestControllerTest.java
+++ shareit-gateway/src/test/java/ru/practicum/shareit/request/ItemRequestControllerTest.java
@@ -6,10 +6,12 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.web.servlet.MockMvc;
+import ru.practicum.shareit.TestConfig;
 
 import java.time.LocalDateTime;
 import java.util.Arrays;
@@ -21,6 +23,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(ItemRequestController.class)
+@Import(TestConfig.class)
 class ItemRequestControllerTest {
 
     @Autowired
